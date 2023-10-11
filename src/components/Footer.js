cebb02b48d74f67ad3609c55ed21dcd751abc6b1
Christopher Koetzee <ckoetzee@southey.co.za>
2023-10-11 13:13:00 +0200

    footer done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,57 +1,76 @@
-import { Row, Col, Button, Card } from "react-bootstrap";
+import React from 'react';
+import {
+  MDBFooter,
+  MDBContainer,
+  MDBIcon,
+  MDBBtn,
+  MDBRow, 
+  MDBCol,
+} from 'mdb-react-ui-kit';
 
-const Footer = () => {
-    return (
-        <div className="footer" 
-            style={{
-                width: "100%", 
-                paddingTop: "3px", 
-                marginTop: "3px"
-                }}>
-            <Card>
-                <Card.Body>
-                    <Row>
-                        <Col>
-                            <Card.Title>Contact Information</Card.Title>
-                            <Card.Text>
-                                <ul>
-                                    <li>Contact Eathan on +27 74 963 4583</li>
-                                    <li>Contact Karien on +27 74 217 0023</li>
-                                    <li>Email address : brostidyandclean02@gmail.com</li>
-                                </ul>
-                            </Card.Text>
-                        </Col>
-                        <Col>
-                        <Card.Title>Social Media</Card.Title>
-                            <Card.Text>
-                                <ul>
-                                    <li>Facebook</li>
-                                    <li>Instagram</li>
-                                    <li>Twitter</li>
-                                </ul>
-                            </Card.Text>
-                        </Col>
-                    </Row> 
-                    <Row>
-                        <Col>
-                            <Button variant="primary">Email us</Button>
-                        </Col>
-                        <Col>
-                            <Button variant="success">Add any comments</Button>
-                        </Col>
-                    </Row> 
-                    {/* <Row>
-                        <Col>
-                            <p>Terms and conditions</p>
-                        </Col>
-                        <Col>
-                            <p>Created by TnT Development</p>
-                        </Col>
-                    </Row>                   */}
-                </Card.Body>
-            </Card>
-        </div>
-    )
-};
+export default function App() {
+  return (
+    <MDBFooter className='text-center text-navy' style={{ backgroundColor: '#61dafb' }}>
+      <MDBContainer className='p-4 pb-0'>
+        <section className='mb-4'>
+            <div className='me-5 d-none d-lg-block'>
+                <MDBRow>
+                    <h4><strong>Contact Us</strong></h4>
+                    <MDBCol>
+                        <p><strong>Get connected with us on social networks : </strong></p>
+                        <div style={{ display: 'flex' }} className='justify-content-center'>
+                        <MDBBtn
+                            floating
+                            className='m-1 d-flex justify-content-center align-items-center'
+                            style={{ backgroundColor: '#3b5998'}}
+                            href='https://www.facebook.com/profile.php?id=100095054632468&mibextid=LQQJ4d'
+                            role='button'
+                        >
+                            <MDBIcon fab icon='facebook-f'/>
+                        </MDBBtn>
+                        <MDBBtn
+                            floating 
+                            className='m-1 d-flex justify-content-center align-items-center'
+                            style={{ backgroundColor: '#55acee' }}
+                            href='https://twitter.com/douglasroadps'
+                            role='button'
+                        >
+                            <MDBIcon fab icon='twitter' />
+                        </MDBBtn>
+                        <MDBBtn
+                            floating
+                            className='m-1 d-flex justify-content-center align-items-center'
+                            style={{ backgroundColor: '#ac2bac' }}
+                            href='https://instagram.com/clean_and_tidy_bros_pty?igshid=MWZjMTM2ODFkZg=='
+                            role='button'
+                        >
+                            <MDBIcon fab icon='instagram' />
+                        </MDBBtn>
+                        </div>
+                    </MDBCol>
+                    <MDBCol>
+                        <p>
+                            <MDBIcon icon="envelope" className="me-3" />
+                            email address : brostidyandclean02@gmail.com
+                        </p>
+                        <p>
+                            <MDBIcon icon="phone" className="me-3" />
+                            Contact Eathan on +27749634583 or Contact Kariem on +27742170023
+                        </p>
+                        <MDBBtn className="ms-auto px-3" >Contact Us</MDBBtn>
+                    </MDBCol>
+                </MDBRow>
+            </div>  
+          
+        </section>
+      </MDBContainer>
 
-export default Footer;
+      <div className='text-center p-3' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
+        © 2023 Copyright : 
+        <a className='text-black' href='https://lustrous-shortbread-942556.netlify.app/'>
+             - Created by TnT Development
+        </a>
+      </div>
+    </MDBFooter>
+  );
+}
